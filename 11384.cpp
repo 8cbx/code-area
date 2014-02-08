b7967c88e836affdc378f48273f4202a27f52d8e
Marylandeny Chu <chu849238686@foxmail.com>
2014-02-08 14:46:58 +0800

    Add 11384

---
+++ 11384.cpp
@@ -0,0 +1,15 @@
+#include<stdlib.h>
+#include<stdio.h>
+int f(int n)
+{
+    return n==1?1:f(n/2)+1;
+}
+int main()
+{
+    int n;
+    while(scanf("%d",&n)!=EOF)
+    {
+        printf("%d\n",f(n));
+    }
+    return 0;
+}
