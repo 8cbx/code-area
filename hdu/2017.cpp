9a76b42709c77721cce736a768360b8b75e8c501
Marylandeny-Chu <chu849238686@foxmail.com>
2014-05-13 01:08:58 +0800

    Add hdu 2017

---
+++ hdu/2017.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    int n;
+    char s;
+    int coun;
+    while(scanf("%d",&n)!=EOF)
+    {
+        while(n--)
+        {
+            scanf("%c",&s);
+            coun=0;
+            while(s!='\n')
+            {
+                if(s>='0'&&s<='9')
+                {
+                    coun++;
+                }
+                scanf("%c",&s);
+            }
+            printf("%d\n",coun);
+        }
+    }
+    return 0;
+}
